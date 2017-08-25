0ef1a246cdf8619d970f6796e9a9fc658cb842cd
russianNinja <itolstof@gmail.com>
2017-08-25 20:26:30 +0300

    merge common block2 from dirty2

---
+++ components/common.blocks/slider/slider.bemhtml.js
@@ -0,0 +1,56 @@
+block('slider').js()( true )
+
+block('slider')(
+  addJs()( ( node, ctx ) => {
+    return {
+      pageCount: ctx.content.length,
+      currentPage: 0
+    }
+  } ),
+
+  content()( ( node, ctx ) => {
+    return [
+      {
+        elem: 'view',
+        content: [
+          {
+            block: 'button',
+            mods: {
+              disabled: true
+            },
+            mix: { block: 'slider', elem: 'prev' }
+          },
+          ctx.content && {
+            elem: 'content',
+            content: ctx.content.map( (page, index) => {
+              return {
+                elem: 'page',
+                elemMods: {
+                  visible: !index
+                },
+                content: page
+              }
+            })
+          },
+          {
+            block: 'button',
+            mix: { block: 'slider', elem: 'next' }
+          }
+        ]
+      },
+      ctx.content && {
+         elem: 'pagination',
+         content: ctx.content.map( (page, index) => {
+           return {
+             elem: 'page-item',
+             elemMods: {
+               active: !index
+             },
+             js: { index: index }
+           }
+         })
+       }
+    ]
+  })
+)
+
---
+++ components/common.blocks/slider/slider.js
@@ -0,0 +1,53 @@
+modules.define('slider',
+  ['i-bem-dom', 'button' ],
+  function(provide, bemDom, Button) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+
+                var pages = this.findChildElems('page');
+                var pagination = this.findChildElems('page-item');
+                this._currentPage = this.params.currentPage;
+                this._pageCount = this.params.pageCount;
+
+                this._domEvents('next').on('click', function(event) {
+                if ( this._currentPage < this._pageCount - 1 ) {
+                    this._elem('prev').findMixedBlock(Button).setMod('disabled', false);
+                    pages.get( this._currentPage ).setMod('visible', false);
+                    pagination.get( this._currentPage ).setMod('active', false);
+                    pagination.get( ++this._currentPage ).setMod('active', true);
+                    pages.get( this._currentPage ).setMod('visible', true);
+                }
+                if ( this._currentPage == this._pageCount - 1) {
+                    event.bemTarget.findMixedBlock(Button).setMod('disabled');
+                }
+                })
+
+                this._domEvents('prev').on('click', function(event) {
+                  if ( this._currentPage > 0 ) {
+                    this._elem('next').findMixedBlock(Button).setMod('disabled', false);
+                    pages.get( this._currentPage ).setMod('visible', false);
+                    pagination.get( this._currentPage ).setMod('active', false);
+                    pagination.get( --this._currentPage ).setMod('active', true);
+                    pages.get( this._currentPage ).setMod('visible', true);
+                  }
+                  if ( this._currentPage == 0) {
+                    this._elem('prev').findMixedBlock(Button).setMod('disabled');
+                  }
+                })
+
+                // this._domEvents('page-item').on('click', function(event) {
+                //   pages.get( this._currentPage ).setMod('visible', false);
+                //   pagination.get( this._currentPage ).setMod('active', false);
+                // }
+
+            }
+        }
+    },
+
+}));
+
+});
+
