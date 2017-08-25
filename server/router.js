be0f144e407fc18b343f92110f30ee9cab9f6b03
leonid <leon@thatsme.ru>
2017-08-25 18:30:33 +0300

    add locale for sort

--- server/router.js
+++ server/router.js
@@ -47,7 +47,7 @@ module.exports = function( app ) {
   request( { url: '/api/category/?per-page=1000'} ).then( response => {
 
     data.category = response.items.sort(function (a,b) {
-      return a.name.localeCompare(b.name);
+      return a.name.localeCompare(b.name, 'ru');
     });
 
     for ( let i = data.category.length - 1; i >= 0; i-- ) {
