78cf044f50eafec3235ce71d4eb568ac2cf52a7a
leonid <leon@thatsme.ru>
2017-08-29 16:12:59 +0300

    host: preprod

--- components/common.blocks/card-movie/_view/card-movie_view_full.js
+++ components/common.blocks/card-movie/_view/card-movie_view_full.js
@@ -8,13 +8,13 @@ provide(CardMovie.declMod({ modName: 'view', modVal: 'full' }, {
         this._cover = this._elem('image');
         this._domEvents('slider-item').on( 'click', ( event ) => {
           let image_id = event.bemTarget.params.image_id;
-          
+
           this._elems('slider-item').setMod('checked', false);
           event.bemTarget.setMod('checked', true);
 
           switch ( event.bemTarget.getMod('type') ) {
             case 'image':
-              this._cover.domElem[0].src = 'http://dev.artdoc.media/upload/resize/' + image_id + '/843x474.1875.jpg';
+              this._cover.domElem[0].src = '/upload/resize/' + image_id + '/843x474.1875.jpg';
               this._elem('video-container').setMod('visible', false);
               this._elem('header').setMod('hide', false);
               break;
