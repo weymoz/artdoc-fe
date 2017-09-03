9a4ca7548555d972bda3274ef2cff297aa6463dc
Realetive <realetive@yandex.ru>
2017-09-04 02:37:22 +0300

    Fix date format in ticket

--- components/common.blocks/card-ticket/__user-date/card-ticket__user-date.bemhtml.js
+++ components/common.blocks/card-ticket/__user-date/card-ticket__user-date.bemhtml.js
@@ -1,34 +1,20 @@
 block('card-ticket').elem('user-date')(
 
   content()( ( node, ctx ) => {
-    return [
-      {
-        block: 'text',
-        mods: {
-          format: 'datetime'
-        },
-        format: 'DD MMMM',
-        content: node._time_gmt3
-      },
-      ctx.append
-    ]
-  }),
-
-  match( node => node.mods.view === 'order' ).content()( node => {
-    const template = node.reapply({
-      block: node.block,
-      elem: node.elem,
-      content: [
+    let format = node.mods.view === 'order'
+      ? '[<div class="card-ticket__user-date">' + node.reapply([
         {
+          block: node.block,
           elem: 'day',
           content: ']DD['
         },
         {
+          block: node.block,
           elem: 'month',
           content: ']MMMM[, ] dd['
         }
-      ]
-    });
+      ]) + '</div>]'
+    : 'DD MMMM'
 
     return [
       {
@@ -36,9 +22,10 @@ block('card-ticket').elem('user-date')(
         mods: {
           format: 'datetime'
         },
-        format: '[' + template + ']',
+        format: format,
         content: node._time_gmt3
       },
+      ctx.append
     ]
   })
 
