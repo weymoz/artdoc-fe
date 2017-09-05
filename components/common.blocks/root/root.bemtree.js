42d88606762112034893c47299466cc670b804d2
Realetive <realetive@yandex.ru>
2017-09-05 03:29:32 +0300

    Replace GA to GTag

--- components/common.blocks/root/root.bemtree.js
+++ components/common.blocks/root/root.bemtree.js
@@ -20,6 +20,10 @@ block('root').replace()(function() {
     scripts: [
       {
         elem: 'js',
+        content: '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);})(window,document,\'script\',\'dataLayer\',\'GTM-MKD3FG5\');'
+      },
+      {
+        elem: 'js',
         url: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js'
       },
       {
