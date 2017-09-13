50bd3387df2f4b8e89d7f5448d57b916158f5036
Realetive <realetive@yandex.ru>
2017-09-13 18:23:42 +0300

    Fix fest's labeles size

--- components/common.blocks/news/news.bemtree.js
+++ components/common.blocks/news/news.bemtree.js
@@ -2,15 +2,15 @@ block('news').content()(function() {
     this.ctx.items = [
       {
         date: 1503923685,
-        url: 'https://www.youtube.com/embed/jybw_0ecl0s',
-        image: 'http://img.youtube.com/vi/jybw_0ecl0s/mqdefault.jpg',
+        url: '//www.youtube.com/embed/jybw_0ecl0s',
+        image: '//img.youtube.com/vi/jybw_0ecl0s/mqdefault.jpg',
         linkname: 'youtube.com',
         title: 'Запуск artdoc.media',
         content: '1 сентября по адресу artdoc.media начинает работу синематека документального кино на русском языке. В момент запуска в архиве доступна информация о более чем  1000 фильмов со всего мира.'
       },
       {
         date: 1503923685,
-        url: 'http://www.rigaiff.lv/2017/ru/category/films-cat/artdocfest-riga/',
+        url: '//www.rigaiff.lv/2017/ru/category/films-cat/artdocfest-riga/',
         linkname: 'rigaiff.lv',
         image: '/assets/img/news/riga.png',
         title: 'В Риге в четвертый раз пройдет латвийская сессия Artdocfest/RigaIFF.',
@@ -18,7 +18,7 @@ block('news').content()(function() {
       },
       {
         date: 1503923685,
-        url: 'http://www.lavrdoc.ru',
+        url: '//www.lavrdoc.ru',
         image: '/assets/img/news/lavr.png',
         linkname: 'www.lavrdoc.ru',
         title: '1 сентября завершилось выдвижение работ на соискание Национальной премии "ЛАВР" - Лавровая ветвь',
@@ -26,8 +26,8 @@ block('news').content()(function() {
       },
       {
         date: 1503923685,
-        url: 'http://www.rigaiff.lv/2017/en/riga-iff-forum-2017/',
-        //image: 'http://artdoc.media.com',
+        url: '//www.rigaiff.lv/2017/en/riga-iff-forum-2017/',
+        //image: '//artdoc.media.com',
         linkname: 'rigaiff.lv',
         title: '8 сентября в Риге (Латвия) состоится питчинг независимых проектов для авторов из бывшего СССР. ',
         content: '"Артдокфест" отобрал 19 проектов , которые примут участие в питчинге Artdocfest/RigaIFF, организованном в сотрудничестве с авторитетным европейским индустриальным событием - Baltic Sea Forum for Documentaries . Питчинг пройдет в Риге с 7 по 10 сентября. Представляем проекты, отобранные на питчинг Artdocfest/RigaIFF, проходящий в сотрудничестве с Baltic Sea Forum for Documentaries'
