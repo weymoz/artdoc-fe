b3cbe5c520bf99355a7027775081d9e5ea26c4b3
Realetive <realetive@yandex.ru>
2017-09-22 17:50:11 +0300

    Title → heading

---
+++ components/common.blocks/heading/heading.bemhtml.js
@@ -0,0 +1,25 @@
+block('heading')(
+  mod('size', 'xl')(
+    tag()('h1')
+  ),
+
+  mod('size', 'l')(
+    tag()('h2')
+  ),
+
+  mod('size', 'm')(
+    tag()('h3')
+  ),
+
+  mod('size', 's')(
+    tag()('h4')
+  ),
+
+  mod('size', 'xs')(
+    tag()('h5')
+  ),
+
+  mod('size', 'xs')(
+    tag()('h5')
+  )
+)
