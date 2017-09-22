f000e8ce6da0f8de5c5f287f50c4e9b0e00ba992
Realetive <realetive@yandex.ru>
2017-09-22 17:46:12 +0300

    Update breadcrumbs data

--- components/common.blocks/breadcrumbs/breadcrumbs.bemhtml.js
+++ components/common.blocks/breadcrumbs/breadcrumbs.bemhtml.js
@@ -1,4 +1,86 @@
 block('breadcrumbs')(
+
+  def()( ( node, ctx ) => {
+    return applyNext( { _url: (node._url || ctx.url) } );
+  } ),
+
   addMix()( { block: 'font', mods: { family: 'pt-mono', loaded: true } } ),
-  tag()('nav')
+
+  tag()('nav'),
+
+  match( node => node._url ).content()( node => {
+    const path = node._url.split( '/' );
+    
+    // Fix ended `/` in URL
+    if ( path[ path.length - 1 ] === '' ) {
+      path.pop();
+    }
+
+    let breadcrumbs = path.map( page => {
+      switch (page) {
+        case '':
+          return {
+            elem: 'item',
+            url: '/',
+            content: 'Главная'
+          };
+
+        case 'cinema':
+          return {
+            elem: 'item',
+            elemMods: {
+              active: 'true'
+            },
+            content: 'Онлайн-киносеансы'
+          };
+
+        case 'selection':
+          return {
+            elem: 'item',
+            elemMods: {
+              active: 'true'
+            },
+            content: 'Подборки'
+          };
+
+        case 'about':
+          return {
+            elem: 'item',
+            elemMods: {
+              active: 'true'
+            },
+            content: 'О проекте'
+        };
+
+        case 'club':
+          return {
+            elem: 'item',
+            elemMods: {
+              active: 'true'
+            },
+            content: 'Клуб Artdoc'
+        };
+
+        case 'movie':
+          return [
+            {
+              elem: 'item',
+              url: '/movie',
+              content: 'Все фильмы'
+            },
+            {
+              elem: 'item',
+              elemMods: {
+                active: true
+              },
+              content: this.data.title
+            }
+          ];
+      }
+
+      return true;
+    });
+
+    return breadcrumbs;
+  } )
 )
