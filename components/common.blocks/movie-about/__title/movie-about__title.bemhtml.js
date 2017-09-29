c295cc561a9ffce8495cd2d954a1083c04d93e23
Realetive <realetive@yandex.ru>
2017-09-29 22:01:57 +0300

    Update movie's about block

--- components/common.blocks/movie-about/__title/movie-about__title.bemhtml.js
+++ components/common.blocks/movie-about/__title/movie-about__title.bemhtml.js
@@ -1,15 +1,22 @@
 block('movie-about').elem('title')(
 
+  match( ( node, ctx ) => !ctx.title ).def()(''),
+  
   tag()('h3'),
 
-  match( ( node, ctx ) => ctx.title ).content()( ( node, ctx ) => {
+  addMix()( [
+    { block: 'heading', mods: { caps: true, size: 'xs' } },
+    { block: 'font', mods: { family: 'helvetica-neue-condensed-bold', loaded: true } }
+  ] ),
+
+  content()( ( node, ctx ) => {
     switch ( ctx.title ) {
       case 'info'    : return 'Информация о фильме';
       case 'authors' : return 'Авторы';
       case 'crew'    : return 'Съемочная группа';
       case 'tags'    : return 'Теги';
-      default:
-        return 'Unknow section'
-    }
+      default        : return 'Unknow section';
+    }      
   } )
+
 )
