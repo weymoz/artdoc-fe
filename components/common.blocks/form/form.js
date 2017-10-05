74464b788f17c29434dc9330ad6675d65d4fa338
Realetive <realetive@yandex.ru>
2017-10-05 14:49:55 +0300

    Remove form's prevent submit

--- components/common.blocks/form/form.js
+++ components/common.blocks/form/form.js
@@ -2,8 +2,7 @@ modules.define('form', ['i-bem-dom'], function(provide, bemDom) {
 
 provide(bemDom.declBlock(this.name, {
     _onSubmit: function() {
-      this.__base.apply( this, arguments );
-      this.domElem[0].submit();
+      
     }
 }));
 
