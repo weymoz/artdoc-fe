3866e827a1a80a3dfaea8ac4287d1192e23a573f
Realetive <realetive@yandex.ru>
2017-11-07 18:15:55 +0300

    Update widget

--- static/assets/js/widget/widget.js
+++ static/assets/js/widget/widget.js
@@ -1,19 +1,25 @@
 var thisScript = document.getElementById( 'artdocWidgetScript' );
 var parent = thisScript.parentElement;
 var iframe = document.createElement( 'iframe' );
-iframe.src = thisScript.dataset.url || 'https://artdoc.media/movie/';
+var url = ( window.location.hash && window.location.hash.match(/^#url\=(.*)$/i) ) ? window.location.hash.match(/^#url\=(.*)$/i)[1] : false;
+iframe.src = url || thisScript.dataset.url || 'https://artdoc.media/movie/';
 iframe.width = '100%';
-iframe.height = 0;
+iframe.height = 500;
 iframe.frameBorder = 0;
 iframe.onload = ( function setIframeHeight( e ) {
+  console.log('onload');
   iframe.scrollIntoView();
-  var height = e.target.contentWindow.postMessage( 'FrameHeight', '*' );
+  var message = e.target.contentWindow.postMessage( 'iFrame', '*' );
 } );
 
-parent.insertBefore( iframe, thisScript.nextSibling );
-
 window.addEventListener( 'message', function ( event ) {
+  console.log('get msg');
   if ( event.data.hasOwnProperty( 'FrameHeight' ) ) {
     iframe.style.height = event.data.FrameHeight + 'px';
   }
+  if ( event.data.hasOwnProperty( 'FrameUrl' ) ) {
+    window.location.hash = '#url' + event.data.FrameUrl;
+  }
 });
+
+parent.insertBefore( iframe, thisScript.nextSibling );
