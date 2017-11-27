eecf6c50b1d132016c64f75bb12b2d61c131a3d9
Realetive <realetive@yandex.ru>
2017-11-27 15:24:37 +0300

    Replace BEMTREE to BEMHTML

--- components/common.blocks/list-tickets/list-tickets.bemhtml.js
+++ components/common.blocks/list-tickets/list-tickets.bemhtml.js
@@ -1,3 +1,16 @@
 block('list-tickets')(
-  tag()('ul')
+  tag()('ul'),
+  content()( ( node, ctx ) => {
+    return ctx.tickets.map( ticket => {
+      return {
+        block: 'card-ticket',
+        mods: {
+          view: 'movie'
+        },
+        mix: { block: 'list-tickets', elem: 'item' },
+        ticket: ticket,
+        js: { ticket: ticket, timezoneOffset: 0 }
+      }
+    } );
+  })
 )
