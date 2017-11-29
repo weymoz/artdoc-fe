c6d134185a12cea33c020afeee9e97b531c972d7
Realetive <realetive@yandex.ru>
2017-11-29 12:15:18 +0300

    Add marked to movie's list view

--- components/common.blocks/page-search/page-search.bemtree.js
+++ components/common.blocks/page-search/page-search.bemtree.js
@@ -1,7 +1,17 @@
+var marked = require('marked');
+
 block('page-search').replace()( node => {
 
   delete node.data.api.items.author;
   const result = node.data.api.items || {};
+
+  if ( result.movie && result.movie.length ) {
+    result.movie.forEach( movie => {
+      if ( movie.offlineShow && movie.offlineShow.description ) {
+        movie.offlineShow.description = marked( movie.offlineShow.description );
+      }
+    } )
+  }
 
   return [
     {
