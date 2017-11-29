0ab84e001ea60432a37ce3a617b42db053c60f09
Realetive <realetive@yandex.ru>
2017-11-29 18:49:34 +0300

    Add share link to detail selection

--- components/common.blocks/card-selection/_view/card-selection_view_detail.bemhtml.js
+++ components/common.blocks/card-selection/_view/card-selection_view_detail.bemhtml.js
@@ -4,42 +4,86 @@ block('card-selection').mod('view', 'detail')(
     return applyNext({ author_image: 'l' });
   }),
 
-  content()( () => {
+  content()( node => {
+    console.log( node );
     return [
         {
-          elem: 'content',
-            content: [
-              {
-                elem: 'cover',
-                width: 1024,
-                height: 250,
-                content: [
-                  // {
-                  //   block: 'breadcrumbs',
-                  //   mix: [
-                  //     { block: 'page', elem: 'content' },
-                  //     { block: node.block, elem: 'header' },
-                  //   ],
-                  //   url: '/selection/' + node._code
-                  // },
-                  {
-                    elem: 'footer',
-                    mix: { block: 'page', elem: 'content' },
-                    content: [
-                      { elem: 'name' },
-                      { elem: 'counter' }
-                    ]
-                  }
-                ]
-              },
-            ]
+          elem: 'header',
+          content: [
+            {
+              elem: 'cover',
+              width: 1024,
+              height: 250,
+              content: [
+                // {
+                //   block: 'breadcrumbs',
+                //   mix: [
+                //     { block: 'page', elem: 'content' },
+                //     { block: node.block, elem: 'header' },
+                //   ],
+                //   url: '/selection/' + node._code
+                // },
+                {
+                  elem: 'footer',
+                  mix: { block: 'page', elem: 'content' },
+                  content: [
+                    { elem: 'name' },
+                    { elem: 'counter' }
+                  ]
+                }
+              ]
+            },
+          ]
         },
         {
-          elem: 'aside',
+          elem: 'section',
           mix: { block: 'page', elem: 'content' },
           content: [
-            { elem: 'author' },
-            { elem: 'description' }
+            {
+              elem: 'content',
+              content: [
+                { elem: 'author' },
+                { elem: 'description' }
+              ]
+            },
+            {
+              elem: 'aside',
+              content: {
+                elem: 'share',
+                content: [
+                  {
+                    block: 'link',
+                    mix: { block: node.block, elem: 'share-item' },
+                    url: '//vk.com/share.php?url=https://artdoc.media/',
+                    target: '_blank',
+                    content: {
+                      block: 'icon',
+                      mods: { social: 'vkontakte', size: 'xxl' },
+                    }
+                  },
+                  {
+                    block: 'link',
+                    mix: { block: node.block, elem: 'share-item' },
+                    url: '//www.facebook.com/sharer.php?u=https://artdoc.media/',
+                    target: '_blank',
+                    content: {
+                      block: 'icon',
+                      mods: { social: 'facebook', size: 'xxl' },
+                    }
+                  },
+                  {
+                    block: 'link',
+                    mix: { block: node.block, elem: 'share-item' },
+                    url: '//twitter.com/share?url=https://artdoc.media/',
+                    target: '_blank',
+                    content: {
+                      block: 'icon',
+                      mods: { social: 'twitter', size: 'xxl' },
+                    }
+                  }
+                ]
+              }
+            }
           ]
         }
      ]
