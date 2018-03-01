ab0bb081c289d7303233ef579ea9994d3e23f4d6
russianNinja <itolstof@gmail.com>
2018-03-01 13:23:46 +0300

    page-author

--- components/common.blocks/card-author/_view/card-author_view_detail.bemhtml.js
+++ components/common.blocks/card-author/_view/card-author_view_detail.bemhtml.js
@@ -17,13 +17,13 @@ block('card-author').mod('view', 'detail')(
       {field: 'web', name: 'Сайт'}
       ];
 
-    // let socials = [
-    //   { field: 'fb', icon: 'facebook'  },
-    //   { field: 'yt', icon: 'instagram' },
-    //   { field: 'inst', icon: 'twitter' },
-    //   { field: 'vk', icon: 'vkontakte' },
-    //   { field: 'tw', icon: 'youtube' }
-    // ]
+    let socials = [
+      { field: 'fb', icon: 'facebook'  },
+      { field: 'yt', icon: 'instagram' },
+      { field: 'inst', icon: 'twitter' },
+      { field: 'vk', icon: 'vkontakte' },
+      { field: 'tw', icon: 'youtube' }
+    ]
 
 
     let author_info = data_rows.map(function (elem) {
@@ -78,56 +78,35 @@ block('card-author').mod('view', 'detail')(
     })
 
 
-    // let social_links = socials.map(function (elem) {
+    let social_links = socials.map(function (elem) {
 
-    //   if (!ctx.author[elem.field] || !ctx.author[elem.field][0]) {
-    //     return false;
-    //   }
+      if (!ctx.author[elem.field] || !ctx.author[elem.field][0]) {
+        return false;
+      }
 
-    //   let value = elem.sub_field ? ctx.author[elem.field][0][elem.sub_field] : ctx.author[elem.field];
+      let value = elem.sub_field ? ctx.author[elem.field][0][elem.sub_field] : ctx.author[elem.field];
 
-    //   if( elem.field === 'web' ) {
-    //     value = {
-    //       block: 'link',
-    //       mods: { size: 's' },
-    //       attrs: { target: '_blank' },
-    //       url: 'http://' + ctx.author[elem.field],
-    //       content: {
-    //         elem: 'content',
-    //         mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } },
-    //         content: ctx.author[elem.field]
-    //       }
-    //     }
-    //   }
 
-    //   if( elem.field === 'email' ) {
-    //     value = {
-    //       block: 'link',
-    //       mods: { size: 's' },
-    //       attrs: { target: '_blank' },
-    //       url: 'mailto:' + ctx.author[elem.field],
-    //       content: {
-    //         elem: 'content',
-    //         mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } },
-    //         content: ctx.author[elem.field]
-    //       }
-    //     }
-    //   }
-
-    //   return { elem: 'item',
-    //     content: [
-    //     {
-    //       elem: 'term',
-    //       content: elem.name
-    //     },
-    //     {
-    //       elem: 'definition',
-    //       content: value
-    //     }
-    //   ]
-    //   }
-
-    // })
+      return {
+        block: 'button',
+        mods: {
+          type: 'link',
+          view: 'plain',
+          size: 'l'
+        },
+        mix: { block: 'card-author', elem: 'social-item' },
+        attrs: { target: '_blank' },
+        icon: {
+          block: 'icon',
+          mods: {
+            social: elem.icon,
+            size: 'l',
+            black: true
+          }
+        },
+        url: value
+      }
+    })
 
     return [
       {
@@ -163,10 +142,11 @@ block('card-author').mod('view', 'detail')(
                     content: author_info
                   },
                   {
-
+                    elem: 'social-row',
+                    content: social_links
                   }
                   ]
-              }
+              },
 
             ]
           }
--- components/common.blocks/card-author/_view/card-author_view_detail.deps.js
+++ components/common.blocks/card-author/_view/card-author_view_detail.deps.js
@@ -2,6 +2,9 @@
   shouldDeps: [
     { block: 'breadcrumbs' },
     { block: 'list', mods: { type: 'description'}},
-    { block: 'link'}
+    { block: 'link'},
+    { block: 'button', mods: { type: 'link', view: 'plain', size: 'l' } },
+    { block: 'icon', mods: {social: '*'} }
   ]
 })
+
