e8abb6154c30ebb94ee314718b90d2dc070e0b9e
russianNinja <itolstof@gmail.com>
2018-03-23 12:37:48 +0300

    WIP

--- components/development.blocks/image/_has-resize/image_has-resize.bemhtml.js
+++ components/development.blocks/image/_has-resize/image_has-resize.bemhtml.js
@@ -1,10 +1,10 @@
-block('image').mod('has-resize', true)(
-  match( ( node, ctx ) => ctx.width && ctx.height )(
-    def()( ( node, ctx ) => {
-      ctx.url = '//artdoc.media/upload/resize/' + ctx.url + '/' + ctx.width + 'x' + ctx.height + '.jpg';
-      delete ctx.height;
+// block('image').mod('has-resize', true)(
+//   match( ( node, ctx ) => ctx.width && ctx.height )(
+//     def()( ( node, ctx ) => {
+//       ctx.url = '//artdoc.media/upload/resize/' + ctx.url + '/' + ctx.width + 'x' + ctx.height + '.jpg';
+//       delete ctx.height;
 
-      return applyNext( ctx )
-    } )
-  )
-)
+//       return applyNext( ctx )
+//     } )
+//   )
+// )
