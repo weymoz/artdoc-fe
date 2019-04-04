ec010be8c5b472546b15ebefce2f797b5ac2a8c9
uncleseneca <uncleseneca@gmail.com>
2019-04-04 11:46:33 +0300

    add translations to form

--- src/features/support/Test.jsx
+++ src/features/support/Test.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-
-import { withLanguages, useTranslatedContent } from './i18n'
-import { support as supportContent } from './translations/support'
+import { support as supportContent } from "../../translations/support";
 
 export const Support = withLanguages(() => {
-  const { title } = useTranslatedContent(supportContent)
+    const { title } = useTranslatedContent(supportContent);
 
-  return <h1>{title}</h1>
-})
+    return <h1>{title}</h1>;
+});
 
-const container = document.getElementById('support-page-react-root')
-if (!!container) {
-  render(<Support />, container)
+const container = document.getElementById("support-page-react-root");
+if (container) {
+    render(<Support />, container);
 }
--- src/translations/support.js
+++ src/translations/support.js
@@ -1,6 +1,22 @@
 export const support = {
-  title: {
-    ru: 'Привет',
-    en: 'Hello',
-  }
+  email: {
+    ru: 'Эл. почта',
+    en: 'Email',
+  },
+  accept: {
+    ru: 'Я принимаю',
+    en: 'I accept',
+  },
+  termsConditions: {
+    ru: 'условия пользовательского соглашения',
+    en: 'terms and conditions',
+  },
+  support: {
+    ru: 'Поддержать проект',
+    en: 'Support the project',
+  },
+  pay: {
+    ru: 'Оплата картой',
+    en: 'Pay by card',
+  },
 }
