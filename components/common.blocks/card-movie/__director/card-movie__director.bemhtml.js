b38521dd00ab063e2b61d57020598c28fcb71e30
uncleseneca <uncleseneca@gmail.com>
2019-04-11 15:33:11 +0300

    fix card movie selection layout

--- components/common.blocks/card-movie/__director/card-movie__director.bemhtml.js
+++ components/common.blocks/card-movie/__director/card-movie__director.bemhtml.js
@@ -5,14 +5,13 @@ block('card-movie').elem('director')(
   tag()((node, ctx) => ctx.tag || 'span'),
   addAttrs()({ title: 'Режиссёр' }),
   match(node => node.ctx.movie && node.ctx.movie.authors).content()(node => {
-    return (
-      node.ctx.movie.authors.map(author => {
-        return {
-          block: 'card-author',
-          mods: { size: 'xs', 'hide-image': true, 'no-offsets': true },
-          author: author
-        };
-      })
-    );
+
+    return node.ctx.movie.authors.map(author => {
+      return {
+        block: 'card-author',
+        mods: { size: 'xs', 'hide-image': true, 'no-offsets': true },
+        author: author
+      };
+    });
   })
 );
--- components/common.blocks/card-movie/_view/card-movie_view_selection.bemhtml.js
+++ components/common.blocks/card-movie/_view/card-movie_view_selection.bemhtml.js
@@ -1,39 +1,47 @@
 block('card-movie').mod('view', 'selection')(
-
-  def()( ( node, ctx ) => {
+  def()((node, ctx) => {
     ctx.movie.cover = ctx.movie.cover || {};
     let lang = ctx.lang || node._lang;
 
-    return applyNext( {
+    return applyNext({
       'ctx.movie.cover.width': 196,
-      'ctx.movie.url': ctx.movie.code ? '/' + lang + '/movie/' + ctx.movie.code : null
-    } );
+      'ctx.movie.url': ctx.movie.code
+        ? '/' + lang + '/movie/' + ctx.movie.code
+        : null
+    });
   }),
 
-  content()( () => {
+  content()((node) => {
     return [
       {
         elem: 'content',
         elemMods: { type: 'link' },
-        content: [
-          { elem: 'image' },
-        ]
+        content: [{ elem: 'image' }]
       },
       {
         elem: 'content',
         content: [
-          { elem: 'director', mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } } },
-          { elem: 'name', elemMods: { size: 'xs', link: true } }
+          { elem: 'name', elemMods: { size: 'xs', link: true, offsetTop: true } },
+          {
+            elem: 'director',
+            mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } },
+            movie: node.ctx.movie
+          },
         ]
       },
       {
         elem: 'aside',
         content: [
-          { elem: 'countries', mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } } },
-          { elem: 'year', mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } } }
+          {
+            elem: 'countries',
+            mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } }
+          },
+          {
+            elem: 'year',
+            mix: { block: 'font', mods: { family: 'pt-mono', loaded: true } }
+          }
         ]
       }
     ];
   })
-
 );
