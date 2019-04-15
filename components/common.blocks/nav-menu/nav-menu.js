96187cd3c18b36054e166b74dd5e3af273d69156
uncleseneca <uncleseneca@gmail.com>
2019-04-15 15:00:59 +0300

    fix styles

--- components/common.blocks/nav-menu/nav-menu.js
+++ components/common.blocks/nav-menu/nav-menu.js
@@ -1,55 +1,59 @@
-modules.define('nav-menu',
+modules.define(
+  'nav-menu',
 
-['i-bem-dom', 'dropdown', 'page', 'header'],
+  ['i-bem-dom', 'dropdown', 'page', 'header'],
 
-function(provide, bemDom, navMenu, Page, Header) {
-
-provide(bemDom.declBlock(this.name, {
-
-    beforeSetMod: {
-        js: {
-          inited: function() {
-
-            if (window.innerWidth > 500){
-              let currentUrl = window.location.pathname;
-              var regMovie = /\/(en|ru)\/movie/i;
-              var regSelection = /\/(en|ru)\/selection/i;
-              var regCinema = /\/(en|ru)\/cinema/i;
-              let isMovie = regMovie.test(currentUrl);
-              let isSelection = regSelection.test(currentUrl);
-              let isCinema = regCinema.test(currentUrl);
-              let links = this.findChildElems('item');
-                if (isMovie){
-                  links.get(0).setMod('checked')
+  function(provide, bemDom, navMenu, Page, Header) {
+    provide(
+      bemDom.declBlock(this.name, {
+        beforeSetMod: {
+          js: {
+            inited: function() {
+              if (window.innerWidth > 500) {
+                let currentUrl = window.location.pathname;
+                var regMovie = /\/(en|ru)\/movie/i;
+                var regSelection = /\/(en|ru)\/selection/i;
+                var regCinema = /\/(en|ru)\/cinema/i;
+                var regSuppport = /\/(en|ru)\/support/i;
+                let isMovie = regMovie.test(currentUrl);
+                let isSelection = regSelection.test(currentUrl);
+                let isCinema = regCinema.test(currentUrl);
+                let isSupport = regSuppport.test(currentUrl);
+                let links = this.findChildElems('item');
+                if (isMovie) {
+                  links.get(0).setMod('checked');
                 } else if (isSelection) {
-                  links.get(1).setMod('checked')
-                } else if (isCinema){
-                  links.get(2).setMod('checked')
+                  links.get(1).setMod('checked');
+                } else if (isCinema) {
+                  links.get(2).setMod('checked');
+                } else if (isSupport) {
+                  links.get(3).setMod('checked');
                 }
-
+              }
             }
           }
-        }
-    },
+        },
 
-
-    onSetMod: {
-        js: {
+        onSetMod: {
+          js: {
             inited: function() {
-
-
-                this._menu = this.findParentBlock( Page )
-                  .findChildBlock( Header )
-                  .findChildBlock( { block: navMenu, modName: 'nav-menu', modVal: true } );
-                  this._domEvents( this._elem('close') ).on('click', this._openMenu );
+              this._menu = this.findParentBlock(Page)
+                .findChildBlock(Header)
+                .findChildBlock({
+                  block: navMenu,
+                  modName: 'nav-menu',
+                  modVal: true
+                });
+              this._domEvents(this._elem('close')).on('click', this._openMenu);
             }
+          }
+        },
+
+        _openMenu: function() {
+          console.log(this._menu);
+          this._menu.delMod('opened');
         }
-    },
-
-    _openMenu: function () {
-      console.log(this._menu);
-      this._menu.delMod('opened');
-    }
-}));
-
-});
+      })
+    );
+  }
+);
